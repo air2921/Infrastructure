89b85abb1a6acb1b1a58d3f6a3a76ce23c879bd3
Alexandr Korikov <air262921@gmail.com>
2025-02-28 13:20:50 +0300

    Добавлена поддержка несколько контекстов базы данных

--- Infrastructure/Services/EntityFramework/TransactionFactory.cs
+++ Infrastructure/Services/EntityFramework/TransactionFactory.cs
@@ -4,10 +4,16 @@ using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Infrastructure.Services.EntityFramework;
 
-public class TransactionFactory<TDbContext>(TDbContext dbContext) : ITransactionFactory where TDbContext : DbContext
+public class TransactionFactory<TDbContext>(TDbContext dbContext) :
+    ITransactionFactory,
+    ITransactionFactory<TDbContext>
+    where TDbContext : DbContext
 {
     public IDbContextTransaction Begin() => dbContext.Database.BeginTransaction();
-
     public async Task<IDbContextTransaction> BeginAsync(CancellationToken cancellationToken = default)
         => await dbContext.Database.BeginTransactionAsync(cancellationToken);
+
+    IDbContextTransaction ITransactionFactory<TDbContext>.Begin() => Begin();
+    async Task<IDbContextTransaction> ITransactionFactory<TDbContext>.BeginAsync(CancellationToken cancellationToken)
+        => await BeginAsync(cancellationToken);
 }
