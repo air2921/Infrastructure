a8a4cfca8756b7718482ba8eab6b0e427391aaca
Alexandr Korikov <air262921@gmail.com>
2025-05-02 14:53:02 +0300

    Удалены неиспользуемые пакеты

--- src/Options/EntityFrameworkConfigureOptions.cs
+++ src/Options/EntityFrameworkConfigureOptions.cs
@@ -8,24 +8,9 @@ namespace Infrastructure.Options;
 public sealed class EntityFrameworkConfigureOptions : Validator
 {
     /// <summary>
-    /// Gets or sets the connection string for the database.
-    /// </summary>
-    /// <value>The connection string for the database.</value>
-    public string Connection { internal get; set; } = null!;
-
-    /// <summary>
     /// Validates whether the instance is configured correctly.
     /// </summary>
     /// <returns><c>true</c> if the configuration is valid; otherwise, <c>false</c>.</returns>
-    /// <remarks>
-    /// The configuration is considered valid if:
-    /// - The <see cref="Connection"/> property is not null or empty.
-    /// </remarks>
     public override bool IsValidConfigure()
-    {
-        if (string.IsNullOrWhiteSpace(Connection))
-            return false;
-
-        return true;
-    }
+        => true;
 }
